c11428b3407d170426004c5650ce66ac10419024
kamalanandh <arukamalaws@gmail.com>
2023-10-29 22:25:45 -0500

    First version

--- src/DesignPatterns/abstractFactoryDesign/EmailAbstractNotification.java
+++ src/DesignPatterns/abstractFactoryDesign/EmailAbstractNotification.java
@@ -4,9 +4,9 @@ class EmailAbstractNotification extends NotificationAbstractService {
 
     private final String networkType;
 
-    //It is called no Argument Construtor - When calls loads, this contructor is called
+    //It is called no Argument Constructor - When calls loads, this constructor is called
     EmailAbstractNotification(NetworkType type){
-        //Super key calls DesignPatterns.abstractFactoryDesign.NotificationAbstractService's contructor with 1 Enum input
+        //Super key calls DesignPatterns.abstractFactoryDesign.NotificationAbstractService's constructor with 1 Enum input
         super(NotificationType.EMAIL, type);
         networkType = type.toString();
         //This method call in turn calls the below method in line number 17
