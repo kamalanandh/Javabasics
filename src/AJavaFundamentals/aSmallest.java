befc1ccc2f50c18b6b3f2a184b804dbf770e487f
kamalanandh <arukamalaws@gmail.com>
2023-11-04 20:42:43 -0500

    formatted

--- src/AJavaFundamentals/aSmallest.java
+++ src/AJavaFundamentals/aSmallest.java
@@ -1,7 +1,8 @@
 package AJavaFundamentals;
 
 public class aSmallest {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("MySmall program");
     }
 }
